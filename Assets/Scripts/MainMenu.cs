db6765ed9b54c39f2168070ea60a3f71bfeaa7af
nichard101 <nickbrown7567@gmail.com>
2024-05-10 23:20:48 +1200

    Updated main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
    
+   public Animator transition;
    void Start(){
         
    }
@@ -12,5 +13,19 @@ public class MainMenu : MonoBehaviour {
    public void StartGame(){
       GameMaster.ResetGame();
       SceneManager.LoadScene("Level");
+   }
+
+   public void GoCredits(){
+      transition.ResetTrigger("MoveToMenu");
+      transition.SetTrigger("MoveToCredits");
+   }
+
+   public void GoMenu(){
+      transition.ResetTrigger("MoveToCredits");
+      transition.SetTrigger("MoveToMenu");
+   }
+
+   public void QuitGame(){
+      Application.Quit();
    }
 }
